3ada4ecf8fb7ff3d03c1bd92e87c832200b96e02
aereal <aereal@aereal.org>
2023-03-28 15:37:58 +0900

    feat: implement deploy subcommand

--- go.mod
+++ go.mod
@@ -3,12 +3,27 @@ module github.com/aereal/frontier
 go 1.20
 
 require (
+	github.com/aws/aws-sdk-go-v2/config v1.18.19
+	github.com/aws/aws-sdk-go-v2/service/cloudfront v1.26.2
 	github.com/urfave/cli/v2 v2.25.1
 	go.uber.org/zap v1.24.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.17.7 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.18 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.31 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.32 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.18.7 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"io"
 
+	"github.com/aereal/frontier"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
 	cliv2 "github.com/urfave/cli/v2"
 	"go.uber.org/zap"
 )
@@ -25,7 +28,38 @@ func New(logger *zap.Logger, input io.Reader, out, errOut io.Writer) *App {
 	if logger != nil {
 		app.logger = logger
 	}
+	app.base.Commands = append(app.base.Commands, &cliv2.Command{
+		Name:   "deploy",
+		Flags:  []cliv2.Flag{flagConfigPath, flagPublish},
+		Action: app.actionDeploy,
+	})
 	return app
+}
+
+var (
+	flagConfigPath = &cliv2.PathFlag{
+		Name:  "config",
+		Usage: "config file path",
+		Value: cliv2.Path("function.yml"),
+	}
+	flagPublish = &cliv2.BoolFlag{
+		Name:  "publish",
+		Usage: "whether publish the function immediately",
+		Value: true,
+	}
+)
+
+func (a *App) actionDeploy(cliCtx *cliv2.Context) error {
+	ctx := cliCtx.Context
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return err
+	}
+	client := cloudfront.NewFromConfig(cfg)
+	configPath := cliCtx.Path(flagConfigPath.Name)
+	doPublish := cliCtx.Bool(flagPublish.Name)
+	deployer := frontier.NewDeployer(client, a.logger)
+	return deployer.Deploy(ctx, configPath, doPublish)
 }
 
 func (a *App) Run(ctx context.Context, args []string) error {
