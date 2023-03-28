8de9580d0ddcfd785b9b4539a9d07463b73d0316
aereal <aereal@aereal.org>
2023-03-28 13:16:58 +0900

    feat: add logger

--- cmd/frontier/main.go
+++ cmd/frontier/main.go
@@ -5,10 +5,39 @@ import (
 	"os"
 
 	"github.com/aereal/frontier/internal/cli"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func main() {
-	if err := cli.New(os.Stdin, os.Stdout, os.Stderr).Run(context.Background(), os.Args); err != nil {
+	if err := run(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func run() error {
+	logger, err := zap.Config{
+		Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+		Encoding:         "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			TimeKey:        "time",
+			LevelKey:       "severity",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.CapitalLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.MillisDurationEncoder,
+		},
+	}.Build()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = logger.Sync()
+	}()
+	if err := cli.New(logger, os.Stdin, os.Stdout, os.Stderr).Run(context.Background(), os.Args); err != nil {
+		return err
+	}
+	return nil
 }
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -5,19 +5,25 @@ import (
 	"io"
 
 	cliv2 "github.com/urfave/cli/v2"
+	"go.uber.org/zap"
 )
 
 type App struct {
-	base *cliv2.App
+	base   *cliv2.App
+	logger *zap.Logger
 }
 
-func New(input io.Reader, out, errOut io.Writer) *App {
+func New(logger *zap.Logger, input io.Reader, out, errOut io.Writer) *App {
 	app := &App{
+		logger: zap.NewNop(),
 		base: &cliv2.App{
 			Reader:    input,
 			Writer:    out,
 			ErrWriter: errOut,
 		},
+	}
+	if logger != nil {
+		app.logger = logger
 	}
 	return app
 }
