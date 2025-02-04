c669ddcfab62e13b7708228340c96f1d3ba0bf63
aereal <trasty.loose@gmail.com>
2025-02-04 11:55:00 +0900

    test: generate mock

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,3 +1,5 @@
+//go:generate go run go.uber.org/mock/mockgen -build_constraint !live -typed -write_command_comment=false -write_package_comment=false -write_source_comment=false -package cli -destination ./command_mock_gen.go github.com/aereal/frontier/internal/cli DeployController,ImportController,RenderController
+
 package cli
 
 import (
